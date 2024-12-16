be6ed29e01a53429ab069a8fc6eed5494f9ea3a7
Tomas Bernotas <email@email.com>
2024-12-16 20:18:26 +0200

    pridedamas masyvas su klausimais ir atsakymais

---
+++ app.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+      question: "What is the capital of France?",
+      choices: ["Berlin", "Madrid", "Paris", "Lisbon"],
+      correctAnswerIndex: 2,
+    },
+    {
+      question: "What is 2 + 2?",
+      choices: ["3", "4", "5", "6"],
+      correctAnswerIndex: 1,
+    },
+    {
+      question: "What is the capital of Lithuania?",
+      choices: ["Berlin", "Kaunas", "Vilnius", "Panevezys"],
+      correctAnswerIndex: 2,
+    },
+    {
+        question: "What is 2 + 5?",
+        choices: ["3", "4", "5", "7"],
+        correctAnswerIndex: 3,
+    },
+    {
+        question: "What is biggest river of Lithuania?",
+        choices: ["Nevezis", "Sventoji", "Nemunas", "Neris"],
+        correctAnswerIndex: 2,
+    },
+    {
+        question: "What is 5 * 5",
+        choices: ["3", "20", "10", "25"],
+        correctAnswerIndex: 3,
+    },
+    {
+        question: "What color is grass?",
+        choices: ["blue", "red", "green", "yellow"],
+        correctAnswerIndex: 2,
+    },
+    {
+        question: "What is 50 / 10?",
+        choices: ["1", "4", "5", "6"],
+        correctAnswerIndex: 2,
+    },
+    {
+        question: "What is largest planet in Solar system?",
+        choices: ["Mars", "Earth", "Jupiter", "Venus"],
+        correctAnswerIndex: 2,
+    },
+    {
+        question: "What is 60 - 20?",
+        choices: ["30", "40", "50", "60"],
+        correctAnswerIndex: 1,
+    },    
+  ];
